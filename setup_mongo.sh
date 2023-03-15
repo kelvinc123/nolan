8e5a5ccdd16872de9a453cd4ca98839cd21f17a5
Kelvin Christian <kelvinchristian277@gmail.com>
2023-03-15 12:03:03 -0700

    first commit

---
+++ setup_mongo.sh
@@ -0,0 +1,10 @@
+# pull mongo image
+docker pull mongo
+
+# create container
+docker stop mongo_nolan
+docker rm mongo_nolan
+docker create -it --name mongo_nolan -p 5100:27010 mongo
+
+# run container
+docker start mongo_nolan
